2123730cefc27a192ad5f9d8082d3b434e1771b3
PedroDpsweb <32308504+PedroDpsweb@users.noreply.github.com>
2018-01-30 20:48:36 +0100

    update

--- js/js.js
+++ js/js.js
@@ -5,7 +5,7 @@ xhttp.onreadystatechange = function() {
   gestionarXml(this);
  }
 };
-xhttp.open("GET", "http://path.to/preguntes.xml", true); //localhost no ha de funcionar
+xhttp.open("GET", "https://rawgit.com/PedroDpsweb/FormularioPedroSanchez/master/XML/xml.xml", true); //localhost no ha de funcionar
 xhttp.send();
 
 function gestionarXml(dadesXml){
